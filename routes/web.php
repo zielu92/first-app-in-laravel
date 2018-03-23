7f05109d2d7ff6f3348b9cfd4980660a027bcccb
zielu92 <zielu0@gmail.com>
2018-03-23 10:53:43 +0100

    added disqus system and fixed bug with comment replies

--- routes/web.php
+++ routes/web.php
@@ -76,3 +76,9 @@ Route::group(['middleware'=>'admin'], function() {
     ]]);
 
 });
+
+Route::group(['middleware'=>'auth'], function() {
+
+    Route::post('comment/reply', 'CommentRepliesController@createReply');
+
+});
